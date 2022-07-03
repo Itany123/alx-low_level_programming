1b0e897d968f1948fc3de1aa8279ba3d6d882d68
Itany <itanysolo@gmail.com>
2022-07-03 03:02:25 -0700

    an thing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,22 @@
-#incl:
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - Enty point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+printf ("%d  is negaitive\n", n);
+else if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is Zero\n", n);
+return (0);
+}
