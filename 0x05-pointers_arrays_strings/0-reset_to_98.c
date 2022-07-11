1c783cb9073c41e5c0f5facbd119ca2d844f525d
Itany <itanysolo@gmail.com>
2022-07-11 10:27:21 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98-function that takes apointer to an int as parameter and
  * updates the value it points to be 98.
  *
- * on: this is the entry
+ * @n: this is the entry
  */
 void reset_to_98(int *n)
 {
